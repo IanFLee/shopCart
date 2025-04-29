fc284ca6cce92e06e24b6a104f9eca9423249d87
user <ianforrestlee@gmail.com>
2025-04-29 16:26:59 -0700

    Add Product List

--- src/Components/ProductShelf.tsx
+++ src/Components/ProductShelf.tsx
@@ -1,12 +1,49 @@
 import Card from './Card.tsx'
 
-export default function ProductShelf () {
 	// Fetch API Data
 	
-	// Generate List of 'Product' Components
+	const products = [
+	{
+		'id': 1,
+		'name': 'table',
+		'price': '50',
+	},
+	{
+		'id': 2,
+		'name': 'chair',
+		'price': '25',
+	},
+	{
+		'id': 3,
+		'name': 'chandelier',
+		'price': '100'
+	}
+	]
 	
+	// Generate List of 'Product' Components
+	const ProductList = ( { products } ) => (
+		<ul className="grid grid-cols-1 gap-4">
+			{products.map( (product) => (
+				<li key={product.id} className="border-2 rounded-lg p-4">
+					<h3 className="text-lg font-medium"> { product.name } </h3>
+					<p className="text-gray-600"> { product.price } </p>
+				</li>
+			) ) }
+		</ul>
+	);
+
+
+export default function ProductShelf () {
 
 	
 	// Build and Export All
-	return <Card title="Aisle Twelve - Groceries" font="font-switzer" />
+	return (
+	<>	
+		<Card title="Aisle Twelve - Groceries" font="font-switzer">
+			<ProductList products={products} />
+		</Card>
+	</>
+	)
 }
+
+
